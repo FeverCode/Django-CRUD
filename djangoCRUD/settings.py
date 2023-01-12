67aafab385cefea0e6f66fcd65a6bc4557c03dc3
FeverCode <giddylancs.gl@gmail.com>
2023-01-13 02:31:28 +0300

    CSRF

--- djangoCRUD/settings.py
+++ djangoCRUD/settings.py
@@ -156,4 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
-CSRF_TRUSTED_ORIGINS = ['https://django-crud-app-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://django-crud-app-production.up.railway.app']
